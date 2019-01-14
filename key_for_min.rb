c6cbb35588af7b00ab7bf255b312b1d096b10858
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-14 23:23:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,5 +3,6 @@
 
 def key_for_min_value(name_hash)
   name_array = name_hash.to_a
-  if name_array[0][1]
+  if name_array[0][1] > name_array[1][1] && name_array[0][1] > name_array[2][1]
+    return 
 end
