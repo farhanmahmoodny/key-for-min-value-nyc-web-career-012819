908faaafcf473a043a55d9868e26b2e79429b04f
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-14 23:38:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,5 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  hightest = {}
-  name_array = name_hash.to_a
-  if name_array[0][1] > name_array[1][1] && name_array[0][1] > name_array[2][1]
-    
+  
 end
